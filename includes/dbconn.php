2917dd8f8e47dee1315e65992f113987d4447dad
Devs45 <ayushfrontend@gmail.com>
2025-02-23 14:41:47 +0545

    PHP Hostel Management

---
+++ includes/dbconn.php
@@ -0,0 +1,7 @@
+<?php
+    $dbuser="root";
+    $dbpass="root";
+    $host="127.0.0.1:8889";
+    $db="php_hostelmanagement";
+    $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+?>
---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+&copy; <?php echo date("Y"); ?> - Hostel Management System - by <a href="https://www.shresthabros.com">ShresthaBros</a>
+</footer>
